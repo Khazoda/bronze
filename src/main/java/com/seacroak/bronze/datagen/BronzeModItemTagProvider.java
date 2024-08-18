0ee67d691aa184687be465107ae312087104131a
Khazoda <khazoda@gmail.com>
2024-08-18 18:16:34 +0100

    datagen

--- src/main/java/com/seacroak/bronze/datagen/BronzeModItemTagProvider.java
+++ src/main/java/com/seacroak/bronze/datagen/BronzeModItemTagProvider.java
@@ -16,6 +16,6 @@ public class BronzeModItemTagProvider extends FabricTagProvider.ItemTagProvider 
   @Override
   protected void configure(RegistryWrapper.WrapperLookup arg) {
     getOrCreateTagBuilder(ItemTags.TRIMMABLE_ARMOR)
-        .add(MainRegistry.BRONZE_HELMET, MainRegistry.BRONZE_CHESTPLATE,MainRegistry.BRONZE_LEGGINGS,MainRegistry.BRONZE_BOOTS);
+        .add(MainRegistry.BRONZE_HELMET, MainRegistry.BRONZE_CHESTPLATE, MainRegistry.BRONZE_LEGGINGS, MainRegistry.BRONZE_BOOTS);
   }
 }
--- src/main/java/com/seacroak/bronze/datagen/advancements/BronzeModAdvancementsProvider.java
+++ src/main/java/com/seacroak/bronze/datagen/advancements/BronzeModAdvancementsProvider.java
@@ -2,19 +2,17 @@ package com.seacroak.bronze.datagen.advancements;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricAdvancementProvider;
-import net.minecraft.advancement.AdvancementEntry;
-import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.advancement.Advancement;
 
-import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
 
 public class BronzeModAdvancementsProvider extends FabricAdvancementProvider {
-  public BronzeModAdvancementsProvider(FabricDataOutput dataGenerator, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
-    super(dataGenerator, registryLookup);
+  public BronzeModAdvancementsProvider(FabricDataOutput dataGenerator) {
+    super(dataGenerator);
   }
 
   @Override
-  public void generateAdvancement(RegistryWrapper.WrapperLookup registryLookup, Consumer<AdvancementEntry> consumer) {
+  public void generateAdvancement(Consumer<Advancement> consumer) {
     new BronzeAdvancements().accept(consumer);
   }
 }
